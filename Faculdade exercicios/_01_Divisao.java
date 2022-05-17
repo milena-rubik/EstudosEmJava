8103b7be4aaedd549d23f9122b3ec719d6df6a76
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2022-05-16 22:01:12 -0300

    Update _01_Divisao.java

--- Faculdade exercicios/_01_Divisao.java
+++ Faculdade exercicios/_01_Divisao.java
@@ -4,8 +4,7 @@ public class _01_Divisao {
         double x; // declara a variavel x como real
         n1 = Integer.parseInt(entrada[0]); // Transforma o argumento / entrada 1 em inteiro (original vem como string)
         n2 = Integer.parseInt(entrada[1]); // Transforma o argumento / entrada 2 em inteiro (original vem como string)
-        x = n1 / n2; // Apesar do x ser double, por dividirmos dois inteiros o resultado é apenas o
-                     // quociente inteiro sem resto
+        x = n1 / n2; // Apesar do x ser double, por dividirmos dois inteiros o resultado é apenas o quociente inteiro sem resto
         System.out.println(x); // imprime no sistema o valor de x
         System.exit(0);
     }
